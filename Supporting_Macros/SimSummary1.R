d9c3c7f31b72a0ea84d139b38e8751cef640810b
Dylan Blanchard <dylblanchard@gmail.com>
2016-06-10 17:03:03 -0500

    fix bug with no numeric inputs/outputs

--- Supporting_Macros/SimSummary1.R
+++ Supporting_Macros/SimSummary1.R
@@ -9,29 +9,37 @@ inputs <- list(
   data = read.Alteryx("#1", mode="data.frame")
 )
 
+numericNames <- colnames(inputs$data)
 
+config$independent <- intersect(numericNames, config$independent)
+config$dependent <- intersect(numericNames, config$dependent)
 
-# Output 1: Scatter Plot
-AlteryxGraph(1, width=576*length(config$independent), height=576*length(config$dependent))
-saScat(inputs$data, config$independent, config$dependent)
-
-# Output 2: Importance Index
-importances <- saImportance(inputs$data, config$independent, config$dependent)
-for (factor in names(importances)) {
-	importances[,factor] <- importances[,factor]
-}
-write.Alteryx(importances, 2)
-
-# Output 3: Pearson's R
-the.Rs <- saR(inputs$data, config$independent, config$dependent)
-for (factor in names(the.Rs)) {
-	the.Rs[,factor] <- as.character(the.Rs[,factor])
-}
-write.Alteryx(the.Rs, 3)
-
-# Output 4: Partial Correlation Coefficients
-the.prs <- as.data.frame(saPr2(inputs$data[,c(config$independent, config$dependent)]))
-for (factor in names(the.prs)) {
-	the.prs[,factor] <- as.character(the.prs[,factor])
-}
-write.Alteryx(the.prs, 4)
+if(length(config$independent) >= 1 &&
+   length(config$dependent) >= 1
+) 
+{
+  # Output 1: Scatter Plot
+  AlteryxGraph(1, width=576*length(config$independent), height=576*length(config$dependent))
+  saScat(inputs$data, config$independent, config$dependent)
+  
+  # Output 2: Importance Index
+  importances <- saImportance(inputs$data, config$independent, config$dependent)
+  for (factor in names(importances)) {
+    importances[,factor] <- importances[,factor]
+  }
+  write.Alteryx(importances, 2)
+  
+  # Output 3: Pearson's R
+  the.Rs <- saR(inputs$data, config$independent, config$dependent)
+  for (factor in names(the.Rs)) {
+    the.Rs[,factor] <- as.character(the.Rs[,factor])
+  }
+  write.Alteryx(the.Rs, 3)
+  
+  # Output 4: Partial Correlation Coefficients
+  the.prs <- as.data.frame(saPr2(inputs$data[,c(config$independent, config$dependent)]))
+  for (factor in names(the.prs)) {
+    the.prs[,factor] <- as.character(the.prs[,factor])
+  }
+  write.Alteryx(the.prs, 4)
+} 
